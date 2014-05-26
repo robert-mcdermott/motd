a66dd7200c209e0da4154092116a01cd9c89f5c2
root <root@frylock.fhcrc.org>
2014-05-26 16:00:19 -0700

    Updated to 1.0

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+maintainer        "Robert McDermott"
+maintainer_email  "rmcdermo@fhcrc.org"
+license           "Apache 2.0"
+description       "Maintains /etc/motd file"
+long_description  <<-EOH
+Maintains /etc/motd file
+EOH
+version           "1.0.2"
+recipe            "motd", "maintains /etc/motd file"
---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+# Author:: Robert McDermott <rmcdermo@fhcrc.org>
+# Cookbook Name:: motd
+# Recipe:: default
+
+template "/etc/motd" do
+  source "motd.erb"
+  group "root"
+  owner "root"
+  mode "0644"
+  backup 0
+end
